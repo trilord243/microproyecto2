a78fc5853dab0070902f393d1b87e24830c00ec2
Felipe Escalona <escalonaf12@gmail.com>
2024-03-10 13:19:54 -0400

    favicon

--- src/features/login-register/LoginPage.jsx
+++ src/features/login-register/LoginPage.jsx
@@ -91,7 +91,11 @@ export default function LoginPage() {
 
             <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 ">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md ">
-
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="https://firebasestorage.googleapis.com/v0/b/microproyecto2-b3bf3.appspot.com/o/vecteezy_heart-rating-review-icon_22057047.png?alt=media&token=b7b9c7b9-eb97-40d6-a764-9c21ce1c25b3"
+                        alt="Your Company"
+                    />
                     <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Inicia Sesion!
                     </h2>
@@ -157,17 +161,16 @@ export default function LoginPage() {
                         <div>
                             <div className="relative mt-10">
                                 <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                                    <div className="w-full border-t -z-10 border-gray-200" />
+                                    <div className="w-full border-t border-gray-200" />
                                 </div>
-                                <div className="relative flex -z-10 justify-center text-sm font-medium leading-6">
-                                    <span className="bg-white px-6 text-gray-900">O inicie sesion  con</span>
+                                <div className="relative flex justify-center text-sm font-medium leading-6">
+                                    <span className="bg-white px-6 text-gray-900">O   Inicie sesion  con </span>
                                 </div>
                             </div>
-
-                            <div className="mt-6 grid grid-cols-2 gap-4">
+                            <div className="mt-6 flex content-center justify-center">
                                 <button
                                     onClick={registerWithGoogle}
-                                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
+                                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-400 focus-visible:ring-transparent"
                                 >
                                     <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
                                         <path
--- src/features/login-register/RegisterPage.jsx
+++ src/features/login-register/RegisterPage.jsx
@@ -128,7 +128,7 @@ export default function RegisterPage() {
                 <div className="sm:mx-auto sm:w-full sm:max-w-md ">
                     <img
                         className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        src="https://firebasestorage.googleapis.com/v0/b/microproyecto2-b3bf3.appspot.com/o/vecteezy_heart-rating-review-icon_22057047.png?alt=media&token=b7b9c7b9-eb97-40d6-a764-9c21ce1c25b3"
                         alt="Your Company"
                     />
                     <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -341,10 +341,10 @@ export default function RegisterPage() {
                                 </div>
                             </div>
 
-                            <div className="mt-6 grid grid-cols-2 gap-4">
+                            <div className="mt-6 flex content-center justify-center gap-4">
                                 <button onClick={registerWithGoogle}
 
-                                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
+                                    className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-400 focus-visible:ring-transparent"
                                 >
                                     <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
                                         <path
